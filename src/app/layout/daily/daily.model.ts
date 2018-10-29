ee27b396ff90f081830a80d93e0c826b94fb2597
Will Sun <will.sun@sap.com>
2018-10-29 17:55:41 +0800

    add task enum type for daily working tracking

---
+++ src/app/layout/daily/daily.model.ts
@@ -0,0 +1,37 @@
+
+export enum ETaskType {
+	WORKING = 'Working',
+	LEARNING = 'Learning',
+	HOBBY = 'Hobby',
+}
+
+export enum ETaskLife {
+	HALF_HOUR = '0.5h',
+	ONE_HOUR = '1h',
+	TWO_HOURs = '2h',
+	THREE_HOURS = '3h',
+	FOUR_HOURS = '4h',
+	HALF_DAY = '4h',
+	ONE_DAY = '1d',
+	ONE_HALF_DAYS = '1.5d',
+	TWO_DAYS = '2d',
+	THREE_DAYS = '3d',
+}
+
+export enum ETaskImportantLevel {
+	 CRITICAL = 'Critical',
+	 MEDIUM = 'medium',
+	 MINOR = 'Minor',
+}
+
+export enum ETaskUrgentLevel {
+	HIGH = 'high',
+	MEDIUM = 'medium',
+	LOW = 'low',
+}
+
+export enum ETaskPlace {
+	OFFICE = 'office',
+	WAY_ON_HOME_COMPANY = 'way on home/company',
+	HOME = 'home',
+}
