e4e8937a6b3b5b062fa5f702a677e62492b2dbfa
Will Sun <will.sun@sap.com>
2018-10-31 17:25:56 +0800

    setup daily page basic layout and import boostrap UI lib

--- src/app/layout/daily/daily.module.ts
+++ src/app/layout/daily/daily.module.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { DailyRoutingModule } from './daily-routing.module';
 import { DailyComponent } from './daily.component';
 import { PageHeaderModule } from '../../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     imports: [
 			CommonModule,
 			FormsModule,
 			DailyRoutingModule,
-			PageHeaderModule],
+			PageHeaderModule,
+			NgbModule.forRoot()],
     declarations: [DailyComponent]
 })
 export class DailyModule {}
